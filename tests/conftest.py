96981e8decb4197ccc6c47a3a498ebfd27027508
Gerrit Schrader <gerrit.schrader@gmail.com>
2021-05-21 08:14:06 +0200

    use temporary database file for unit tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,13 +5,18 @@ from faker import Faker
 from fastapi.testclient import TestClient
 
 from netflix_mock.app import app
+from netflix_mock.settings import get_settings
 
 fake = Faker()
 
 
 @pytest.fixture
-def client():
-    return TestClient(app)
+def client(tmp_path):
+    db_file = tmp_path / "test.db"
+    client = TestClient(app)
+    settings = get_settings()
+    settings.DATABASE_URL = f"sqlite:///{db_file}"
+    return client
 
 
 @pytest.fixture
@@ -22,6 +27,6 @@ def basic_auth():
 @pytest.fixture
 def user():
     return dict(
-        username=fake.pystr(),
-        password=fake.pystr(),
+        username=fake.pystr(min_chars=8, max_chars=16),
+        password=fake.pystr(min_chars=8, max_chars=64),
     )
