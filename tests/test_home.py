2dff29698d0e2938240998b32b3217e980c0f484
ges0909 <gerrit.schrader@gmail.com>
2021-06-01 18:17:46 +0200

    'home' test un-skipped

--- tests/test_home.py
+++ tests/test_home.py
@@ -1,9 +1,7 @@
-import pytest
 from fastapi import status
 
 
-@pytest.mark.skip(reason='assert message["type"] == "http.response.start')
 def test_home(client):
-    response = client.get(path="")
+    response = client.get(url="")
     assert response.status_code == status.HTTP_200_OK
     assert response.text
