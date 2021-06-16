08dd6c4cdd20973a7e9a9f6b07d4a36c65b86478
ges0909 <gerrit.schrader@gmail.com>
2021-06-16 09:43:59 +0200

    minor improvements

--- netflix_mock/middleware/http_logging.py
+++ netflix_mock/middleware/http_logging.py
@@ -9,7 +9,7 @@ logger = logging.getLogger(__name__)
 
 class HttpLogging(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next) -> Response:
-        logger.debug("> %s %s", request.method, request.url.path)
+        logger.debug(">> %s %s", request.method, request.url.path)
         response = await call_next(request)
-        logger.debug("< %s %s %i", request.method, request.url.path, response.status_code)
+        logger.debug("<< %s %s %i", request.method, request.url.path, response.status_code)
         return response
